290adf064c9e7dacbde7f1bffe16761cffc9efe7
denny <denny.syj@hotmail.com>
2017-03-25 20:40:28 +0800

    learn the content security policy

--- devtools/panel/assets/bungo.js
+++ devtools/panel/assets/bungo.js
@@ -1,3 +1,15 @@
+window.onload = function() {
+  show_div('res_info');
+  $('#btn_res_info').click(function(){
+    show_div('res_info');
+  });
+  $('#btn_team_info').click(function(){
+    show_div('team_info');
+  });
+  $('#btn_log_info').click(function(){
+    show_div('log_info');
+  });
+}
 function show_div(div_id){
   $('#res_info').hide();
   $('#team_info').hide();
