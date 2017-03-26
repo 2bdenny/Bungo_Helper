93859c23871c38ab9a3f831bd263d0f708f11ed0
denny <denny.syj@hotmail.com>
2017-03-26 23:08:05 +0800

    reconstructure

--- devtools/panel/assets/bungo.js
+++ devtools/panel/assets/bungo.js
@@ -1,5 +1,23 @@
+/* all game status */
+var GAME_STATUS = {
+  mypage: 1,      // 图书馆
+  select_dive: 2, // 潜书界面
+  workspaces: 3,  // 有魂书界面
+  finish: 4,      // 有魂书结束，结束后会重新调用workspaces
+  // 有魂书开始
+  repair_docks: 5,// 修补界面，推测有给出所有人的id和修补速度
+  repair_docks_one: 10,// 选择一个修补，是一串数字，结束后同样会重新调用repair_docks
+  skill_tree: 6,  // 开花界面，推测有给出所有人的id和开花进展
+  skill_tree_one: 11,// 选择一个进入开花界面，是一串数字
+  deck: 7,        // 结成界面，有所有队伍的信息
+  supply: 8,      // 食堂界面、吃的食物，两种都是这个supply，但是数据有所区别，吃supply之后，会重新调用食堂的supply
+  myroom: 9       // 司书室
+};
 
+/* current game status */
+//var cur_game_status;
 
+/* main */
 window.onload = function() {
   /* UI animation */
   show_div('res_info');
@@ -17,16 +35,10 @@ window.onload = function() {
   chrome.devtools.network.onRequestFinished.addListener(function(request){
     var url = request.request.url;
     var cur_state = route(url);
-    if (/.*mypage$/g.test(url)) {
-      /* Only for test */
-      $('#test').html(url);
-
-      /* Information update */
-      request.getContent(function(content, encoding) {
-        var con = JSON.parse(content);
-        show_data(con);
-      });
-    }
+    request.getContent(function(content, encoding) {
+      var con = JSON.parse(content);
+      show_data(con, cur_state);
+    });
   });
 }
 
@@ -40,12 +52,18 @@ function show_div(div_id){
 
 /* dispatch data here */
 function route(url) {
-
+  $('#test').html(url);
+  if (/.*mypage$/g.test(url)) {
+    return GAME_STATUS.mypage;
+  }
+  if (/.*deck$/g.test(url)) {
+    return GAME_STATUS.deck;
+  }
 }
 
 /* show data here */
-function show_data(con){
-  if (con) {
+function show_data(con, cur_state){
+  if (con && cur_state == GAME_STATUS.mypage) {
     $('#tip').html();
     $('#name').html(con.header.name);
     $('#level').html(con.header.level);
