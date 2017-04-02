7e3eda1c6112f2d41b6ccf75b2b8e0d38ba7b616
2bdenny <denny.syj@hotmail.com>
2017-04-02 23:08:11 +0800

    How to use Vue.js?

--- devtools/panel/assets/bungo.js
+++ devtools/panel/assets/bungo.js
@@ -34,6 +34,9 @@ window.onload = function() {
     show_div('log_info');
   });
 
+  /* TEST ONLY */
+//  TEST_ONLY_show_all();
+
   /* dispatch data */
   chrome.devtools.network.onRequestFinished.addListener(function(request){
     var url = request.request.url;
@@ -43,6 +46,13 @@ window.onload = function() {
       show_data(con, cur_state);
     });
   });
+}
+
+/* TEST ONLY */
+function TEST_ONLY_show_all() {
+    $('#res_info').hide();
+    $('#team_info').show();
+    $('#log_info').hide();
 }
 
 /* UI animation of index.html */
@@ -66,28 +76,73 @@ function route(url) {
 
 /* show data here */
 function show_data(con, cur_state){
-  // 首页信息，包含用户信息、资源信息、信件信息
-  if (con && cur_state == GAME_STATUS.mypage) {
-    // 用户信息
-    $('#name').html(con.header.name);
-    $('#level').html(con.header.level);
-    $('#user_id').html(con.header.user_id);
-
-    // 资源信息
-    $('#res_ink').html(con.header.res_ink);
-    $('#res_food').html(con.header.res_food);
-    $('#item_book').html(con.header.item_book);
-    $('#item_quick').html(con.header.item_quick);
-
-    // 信件信息
-    var sT = con.letter.settings.selectedTime;
-    var sI = con.letter.settings.selectedItem;
-    var unit = ['饭团', '墨汁'];
-    $('#selectedTime').html(con.letter.settings.times[sT]);
-    $('#selectedItem').html(con.letter.settings.items[sI][sT] + unit[sI-1]);
-    $('#letter_num').html(con.letter.check.num);
-    $('#letter_time').html(con.letter.check.time);//TODO: 倒计时
+  if (con && cur_state == GAME_STATUS.mypage) { // 首页信息，包含用户信息、资源信息、信件信息
+    show_mypage(con);
+  } else if (con && cur_state == GAME_STATUS.deck) { // 队伍信息
+    show_deck(con);
   } else {
     // 其他情况
   }
+}
+
+/* 展示首页信息，包含用户信息、资源信息、信件信息 */
+function show_mypage(con) {
+  // 用户信息
+  $('#name').html(con.header.name);
+  $('#level').html(con.header.level);
+  $('#user_id').html(con.header.user_id);
+
+  // 资源信息
+  $('#res_ink').html(con.header.res_ink);
+  $('#res_food').html(con.header.res_food);
+  $('#item_book').html(con.header.item_book);
+  $('#item_quick').html(con.header.item_quick);
+
+  // 信件信息
+  var sT = con.letter.settings.selectedTime;
+  var sI = con.letter.settings.selectedItem;
+  var unit = ['饭团', '墨汁'];
+  $('#selectedTime').html(con.letter.settings.times[sT]);
+  $('#selectedItem').html(con.letter.settings.items[sI][sT] + unit[sI-1]);
+  $('#letter_num').html(con.letter.check.num);
+  $('#letter_time').html(con.letter.check.time);//TODO: 倒计时
+}
+
+/* 展示队伍信息 */
+function show_deck(con) {
+  var teams_data = [];
+  var decks = con.decks;
+  for (var d in decks) {
+    // 获取一个队员的信息
+    var mems = [];
+    var units = decks[d].units;
+    for (var m in units) {
+      var mem = units[m];
+      var amem = {
+        name: mem.master.name,
+        category: mem.master.category,
+        hp: mem.hp,
+        fp: mem.fp,
+        equipment: mem.equipment ? 1:0,
+        sp: mem.sp,
+        lb: mem.lb,
+        next_exp: parseInt(mem.next_level_exp) - parseInt(mem.exp)
+      }
+      mems.push(amem);
+    }
+    // 整合一个队伍的信息
+    var ateam = {
+      id: parseInt(d)+1,
+      name: con.decks[d].name,
+      members: mems
+    };
+    teams_data.push(ateam);
+  }
+
+  var team_app = new Vue({
+    el: '#team_info',
+    data: {
+      teams: teams_data;
+    }
+  });
 }
