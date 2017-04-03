afec805379a23aef23e440b2e11b51eb795f7fcf
2bdenny <denny.syj@hotmail.com>
2017-04-03 23:20:01 +0800

    vue.js worked

--- devtools/panel/assets/bungo.js
+++ devtools/panel/assets/bungo.js
@@ -1,5 +1,15 @@
+
+/* 所有的常量保存在这里 */
+
+// 信件到达周期，时间单位是秒
+var letter_all_times = [ 1800, 3600, 10800, 21600, 43200 ];
+
+// 精神状况
+var master_all_mentals = [ '安定', '丫丫安定', '普通', '丫丫不安定', '不安定' ];
+
 /* all game status */
 // CURRENT MAX 13
+// 这里主要是为了方便，数字可以随便改，只要没重复就行
 var GAME_STATUS = {
   mypage: 1,      // 图书馆
   select_dive: 2, // 潜书界面
@@ -7,18 +17,20 @@ var GAME_STATUS = {
   finish: 4,      // 有魂书结束，结束后会重新调用workspaces
   // 有魂书开始
   repair_docks: 5,// 修补界面，推测有给出所有人的id和修补速度
-  repair_docks_one: 10,// 选择一个修补，是一串数字，结束后同样会重新调用repair_docks
-  skill_tree: 6,  // 开花界面，推测有给出所有人的id和开花进展
-  skill_tree_one: 11,// 选择一个进入开花界面，是一串数字
-  deck: 7,        // 结成界面，有所有队伍的信息
-  supply: 8,      // 食堂界面、吃的食物，两种都是这个supply，但是数据有所区别，吃supply之后，会重新调用食堂的supply
-  myroom: 9,      // 司书室
+  repair_docks_one: 6,// 选择一个修补，是一串数字，结束后同样会重新调用repair_docks
+  skill_tree: 7,  // 开花界面，推测有给出所有人的id和开花进展
+  skill_tree_one: 8,// 选择一个进入开花界面，是一串数字
+  deck: 9,        // 结成界面，有所有队伍的信息
+  supply: 10,      // 食堂界面、吃的食物，两种都是这个supply，但是数据有所区别，吃supply之后，会重新调用食堂的supply
+  myroom: 11,      // 司书室
   letters_open: 12,// 收信
   letters_settings: 13//设置书信时间
 };
 
-/* current game status */
-//var cur_game_status;
+/* 信件信息 */
+var letter_settings_seconds = 0;  // 设置好的信件周期
+var letter_seconds = -1; // 信件倒计时
+var letter_count = 0;   // 信件数目
 
 /* main */
 window.onload = function() {
@@ -34,14 +46,8 @@ window.onload = function() {
     show_div('log_info');
   });
 
-  /* TEST ONLY */
-//  TEST_ONLY_show_all();
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+  /* 信件倒计时 */
+  setInterval(letter_time_countdown, 1000);
 
   /* dispatch data */
   chrome.devtools.network.onRequestFinished.addListener(function(request){
@@ -52,14 +58,22 @@ var app = new Vue({
       show_data(con, cur_state);
     });
   });
+
+/*****************************/
+  /* TEST ONLY */
+  //TEST_ONLY_show_all();
+/*****************************/
+
 }
 
+/*****************************/
 /* TEST ONLY */
 function TEST_ONLY_show_all() {
-    $('#res_info').hide();
-    $('#team_info').show();
-    $('#log_info').hide();
+  $('#res_info').show();
+  $('#team_info').show();
+  $('#log_info').show();
 }
+/*****************************/
 
 /* UI animation of index.html */
 function show_div(div_id){
@@ -67,6 +81,33 @@ function show_div(div_id){
   $('#team_info').hide();
   $('#log_info').hide();
   if(div_id) $('#' + div_id).show();
+}
+
+function letter_time_countdown() {
+  if (letter_seconds > 0) {
+    letter_seconds = letter_seconds - 1;
+    var h = Math.floor(letter_seconds / 3600);
+    var m = Math.floor( (letter_seconds - h * 3600) / 60 );
+    var s = letter_seconds - h * 3600 - m * 60;
+    $('#letter_time').html(h.toString() + ":" + m.toString() + ":" + s.toString());
+  } else if (letter_seconds == 0 ){
+    if (letter_count == 0) {
+      letter_count = letter_count + 1;
+      letter_seconds = letter_settings_seconds - 1;
+      return ;
+    }
+    if (letter_count == 1) {
+      letter_count = letter_count + 1;
+      letter_seconds = - 1;
+      return ;
+    }
+    if (letter_count == 2) {
+      letter_seconds = -1;
+      return ;
+    }
+  } else {
+    $('#letter_time').html("0:0:0");
+  }
 }
 
 /* dispatch data here */
@@ -111,30 +152,29 @@ function show_mypage(con) {
   $('#selectedTime').html(con.letter.settings.times[sT]);
   $('#selectedItem').html(con.letter.settings.items[sI][sT] + unit[sI-1]);
   $('#letter_num').html(con.letter.check.num);
-  $('#letter_time').html(con.letter.check.time);//TODO: 倒计时
+  letter_settings_seconds = letter_all_times[sT];
+  letter_seconds = con.letter.check.time;
+  letter_count = con.letter.check.num;
 }
 
 /* 展示队伍信息 */
 function show_deck(con) {
   var teams_data = [];
-  var decks = con.decks;
   for (var d in con.decks) {
-  for (var d in decks) {
-    // 获取一个队员的信息
     var mems = [];
-    var units = decks[d].units;
-    for (var m in units) {
-      var mem = units[m];
+    // 获取一个队员的信息
+    for (var m in con.decks[d].units) {
       var amem = {
-        name: mem.master.name,
-        category: mem.master.category,
-        hp: mem.hp,
-        fp: mem.fp,
-        equipment: mem.equipment ? 1:0,
-        sp: mem.sp,
-        lb: mem.lb,
-        next_exp: parseInt(mem.next_level_exp) - parseInt(mem.exp)
-      }
+        name: con.decks[d].units[m].master.name,
+        level: con.decks[d].units[m].level,
+        category: con.decks[d].units[m].master.category,
+        hp: con.decks[d].units[m].hp,
+        fp: con.decks[d].units[m].fp,
+        mental: master_all_mentals[con.decks[d].units[m].master.mental],
+        sp: con.decks[d].units[m].sp,
+        lb: con.decks[d].units[m].lb,
+        next_exp: parseInt(con.decks[d].units[m].next_level_exp) - parseInt(con.decks[d].units[m].exp)
+      };
       mems.push(amem);
     }
     // 整合一个队伍的信息
@@ -144,13 +184,12 @@ function show_deck(con) {
       members: mems
     };
     teams_data.push(ateam);
-    teams_data.push(con.decks[d].name)
   }
 
   var team_app = new Vue({
     el: '#team_info',
     data: {
-      teams: teams_data;
+      teams: teams_data
     }
   });
 }
