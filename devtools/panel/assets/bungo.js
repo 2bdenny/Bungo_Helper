28c0cbb4155ecf34a29539a65110aefdc3b8d623
2bdenny <denny.syj@hotmail.com>
2017-04-04 21:17:25 +0800

    fp & lb update ok

--- devtools/panel/assets/bungo.js
+++ devtools/panel/assets/bungo.js
@@ -11,7 +11,7 @@ var master_all_mentals = [ '安定', '丫丫安定', '普通', '丫丫不安定'
 var master_all_categories = [ '刃', '弓', '铳', '鞭' ];
 
 /* all game status */
-// CURRENT MAX 13
+// CURRENT MAX 16
 // 这里主要是为了方便，数字可以随便改，只要没重复就行
 var GAME_STATUS = {
   mypage: 1,      // 图书馆
@@ -27,7 +27,10 @@ var GAME_STATUS = {
   supply: 10,      // 食堂界面、吃的食物，两种都是这个supply，但是数据有所区别，吃supply之后，会重新调用食堂的supply
   myroom: 11,      // 司书室
   letters_open: 12,// 收信
-  letters_settings: 13//设置书信时间
+  letters_settings: 13,//设置书信时间
+  stages: 14, //选择战场
+  battle: 15,  //出阵
+  result: 16  // 结果
 };
 
 /* 信件信息 */
@@ -142,6 +145,15 @@ function route(url) {
   if (/.*deck$/g.test(url)) {
     return GAME_STATUS.deck;
   }
+  if (/.*stages\/start\/\d+/g.test(url)) {
+    return GAME_STATUS.stages;
+  }
+  if (/.*stages\/battle/g.test(url)) {
+    return GAME_STATUS.battle;
+  }
+  if (/.*stages\/result/g.test(url)) {
+    return GAME_STATUS.result;
+  }
 }
 
 /* show data here */
@@ -151,6 +163,12 @@ function show_data(con, cur_state){
   } else if (con && cur_state == GAME_STATUS.deck) { // 队伍信息
     show_deck(con);
     show_bungos(con);
+  } else if (con && cur_state == GAME_STATUS.stages) {
+    update_log_stage(con);
+  } else if (con && cur_state == GAME_STATUS.battle) {
+    update_info_battle(con);
+  } else if (con && cur_state == GAME_STATUS.result) {
+    update_info_result(con);
   } else {
     // 其他情况
   }
@@ -190,6 +208,7 @@ function show_deck(con) {
     for (var m in con.decks[d].units) {
       if (!con.decks[d].units[m]) continue;
       var amem = {
+        id: con.decks[d].units[m].id,
         name: con.decks[d].units[m].master.name,
         level: con.decks[d].units[m].level,
         category: master_all_categories[con.decks[d].units[m].master.category - 1],
@@ -219,6 +238,7 @@ function show_bungos(con) {
   var bungos_data = [];
   for (var d in con.units) {
     var amem = {
+      id: con.units[d].id,
       name: con.units[d].master.name,
       level: con.units[d].level,
       category: master_all_categories[con.units[d].master.category - 1],
@@ -233,4 +253,66 @@ function show_bungos(con) {
   }
 
   bungo_app.bungos = bungos_data;
+}
+
+/* 文豪出阵选择战场 */
+function update_log_stage(con) {
+  // TODO 战场信息，更新log
+}
+
+/* 文豪战斗，更新team数值和文豪列表的数值 */
+function update_info_battle(con) {
+  // 捡到资源
+  if (con.category == 2) return;
+
+  // 战斗
+  if (con.category == 1) {
+    for (var d in con.result.units) {
+      var bungo_id = con.result.units[d].id;
+      var bungo_hp = con.result.units[d].hp;
+
+      update_bungo(bungo_id, 'hp', bungo_hp);
+    }
+  }
+}
+
+/* 在这里更新bungos_data和team_data */
+function update_bungo(id, item, value) {
+  for (var i = 0; i < bungo_app.bungos.length; i++) {
+    if (bungo_app.bungos[i].id == id) {
+      if (item == 'hp') {
+        bungo_app.bungos[i].hp = value;
+      } else if (item == 'fp') {
+        bungo_app.bungos[i].fp = value;
+      } else if (item == 'lb') {
+        bungo_app.bungos[i].lb = value;
+      }
+      break; // 因为拥有的文豪不会重复
+    }
+  }
+  for (var i = 0; i < team_app.teams.length; i++) {
+    for (var j = 0; j < team_app.teams[i].members.length; j++) {
+      if (team_app.teams[i].members[j].id == id) {
+        if (item == 'hp') {
+          team_app.teams[i].members[j].hp = value;
+        } else if (item == 'fp') {
+          team_app.teams[i].members[j].fp = value;
+        } else if (item == 'lb') {
+          team_app.teams[i].members[j].lb = value;
+        }
+        break; // 因为一个队伍里不会有相同的文豪
+      }
+    }
+  }
+}
+
+/* 根据战斗结束的信息对bungos_data和team_data进行更新 */
+function update_info_result(con) {
+  // 更新文豪fp/lb信息
+  for (var d in con.units) {
+    update_bungo(con.units[d].id, 'fp', con.units[d].fp);
+    update_bungo(con.units[d].id, 'lb', con.units[d].lb);
+  }
+
+  // TODO update log
 }
